408d6a08581814e3a78eb0aedfe8124e2e8a9cd5
svd720 <shaurydwivedi720@gmail.com>
2026-06-09 02:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (49.12%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int a=0;int l=1;string late="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='A')
+            a++;
+            if(s[i]=='L')
+            {
+                late+=s[i];
+            }
+            else if(s[i]!='L')
+            {
+                if(late.size()>=3)
+                return false;
+                late="";
+
+            }
+        }
+        if(a>=2||late.size()>=3)
+        return false;
+        return true;
+    }
+};
